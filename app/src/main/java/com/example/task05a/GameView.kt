b98bd761c6700b03a28b7a74d687cbbeaa261ed9
Jakub <jakub.bebenek@gmail.com>
2020-03-08 20:00:31 +0000

    Created Game View class and added code for circle generation.

---
+++ app/src/main/java/com/example/task05a/GameView.kt
@@ -0,0 +1,76 @@
+package com.example.task05a
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import java.security.AccessControlContext
+
+class GameView: View {
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    private val colCount = 7
+    private val rowCount = 10
+
+    private var mGridPaint: Paint
+    private var mNoPlayerPaint: Paint
+
+    init {
+        mGridPaint = Paint().apply {
+            style = Paint.Style.FILL
+            color = Color.BLUE
+        }
+
+        mNoPlayerPaint = Paint().apply {
+            style = Paint.Style.FILL
+            color = Color.WHITE
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        val chosenDiameter: Float
+        var tokenAtPos: Int
+        var paint: Paint
+
+        val viewWidth: Float = width.toFloat()
+        val viewHeight: Float = height.toFloat()
+
+        val diameterX: Float = viewWidth / colCount.toFloat()
+        val diameterY: Float = viewHeight / rowCount.toFloat()
+
+        if (diameterX < diameterY)
+            chosenDiameter = diameterX
+        else
+            chosenDiameter = diameterY
+
+        canvas.drawRect(0.toFloat(), 0.toFloat(), viewWidth, viewHeight, mGridPaint)
+
+        val radius = chosenDiameter / 2
+
+        // Draw the circles on the game board
+        for (col in 0 until colCount) {
+            for (row in 0 until rowCount) {
+
+                paint = mNoPlayerPaint
+
+                // Calculate the coordinates of the circle
+                val cx = chosenDiameter * col + radius
+                val cy = chosenDiameter * row + radius
+
+                canvas.drawCircle(cx, cy, radius, paint)
+            }
+        }
+    }
+}
+
