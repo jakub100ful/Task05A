19c5879d2ab0a0e47997d885a0047eb3fcc84305
Jakub <jakub.bebenek@gmail.com>
2020-03-08 21:01:43 +0000

    Cleanup.

--- app/src/main/java/com/example/task05a/GameView.kt
+++ app/src/main/java/com/example/task05a/GameView.kt
@@ -2,7 +2,6 @@ package com.example.task05a
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
@@ -23,7 +22,7 @@ class GameView: View {
 
     private var mGridPaint: Paint
     private var mNoPlayerPaint: Paint
-
+    
     init {
         mGridPaint = Paint().apply {
             style = Paint.Style.FILL
