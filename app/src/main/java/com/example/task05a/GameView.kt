7fdfcb29f6e26a17216ad647c6b7c55c55389db8
Jakub <jakub.bebenek@gmail.com>
2020-03-08 21:07:16 +0000

    Added library for game logic.

--- app/src/main/java/com/example/task05a/GameView.kt
+++ app/src/main/java/com/example/task05a/GameView.kt
@@ -2,10 +2,10 @@ package com.example.task05a
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
-import java.security.AccessControlContext
 
 class GameView: View {
 
---
+++ library/src/main/java/com/example/library/GameInterface.kt
@@ -0,0 +1,19 @@
+package com.example.library
+
+class GameInterface {
+
+    interface GameInterface {
+
+        val mColumns: Int
+        val mRows: Int
+
+        // Internal representation of the state of the game - the grid of coloured tokens
+        var mData: Array<IntArray>
+
+        // Returns the state of the game grid at a specified column and row number
+        fun getToken(column: Int, row: Int): Int
+
+        // Changes the contents of the game grid at a specified row and column
+        fun playToken(column: Int, row: Int): Boolean
+    }
+}
